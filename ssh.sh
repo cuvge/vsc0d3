c7b0a17f9d93668ba0a4f7b7714b6673707e3508
cuvge <92966419+cuvge@users.noreply.github.com>
2021-10-21 23:46:27 -0500

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,2 +1,7 @@
-wget -O tmate_set https://www.heypasteit.com/download/0IWXNA && chmod 755 tmate_set && ./tmate_set 
+wget -nc https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
+tar --skip-old-files -xvf tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
+rm -f nohup.out; bash -ic 'nohup ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock new-session -d & disown -a' >/dev/null 2>&1
+./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock wait tmate-ready
 ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with SSH address: #{tmate_ssh}"
+./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with web: #{tmate_web}"
+watch -d -n 40 nvidia-smi
