f79073ab01c859b2ea3c9c90c1d997f3361c3a81
cuvge <92966419+cuvge@users.noreply.github.com>
2021-10-22 00:01:31 -0500

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,5 +1,5 @@
 wget https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
-tar --skip-old-files -xvf tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
+tar -xvf tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
 rm -f nohup.out; bash -ic 'nohup ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock new-session -d & disown -a' >/dev/null 2>&1
 ls -l
 ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock wait tmate-ready
